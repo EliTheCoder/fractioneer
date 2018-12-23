c9137028281211641223fb7ab615ae6c6071e77a
elithecoder <ex.games.org@gmail.com>
2018-12-22 20:46:20 -0600

    added basic site

---
+++ server.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const port = 80;
+
+const express = require('express');
+const path = require("path");
+const app = express();
+const util = require('util');
+const pm2io = require('@pm2/io');
+
+app.use(express.static(path.join(__dirname, '/static')));
+
+const server = app.listen(process.env.PORT || port, () => {
+  logMessage(0, "SERVER RUNNING: PORT: " + port);
+});
+
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  eliapi.logMessage(0, "CLIENT CONNECTED WITH IP ADDRESS: " + socket.request.connection.remoteAddress.split(':').slice(3)[0]);
+}
